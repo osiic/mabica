450f4cd3ea6bbd8d6113bc5eb5d1a6a385282f71
osiic <osiic.offcl@gmail.com>
2024-03-30 14:39:51 -0800

    feat(login): edit link to back in home more beautiful [/admin,/login]

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function SignInPage() {
   return (
-    <div className="flex h-screen">
+    <div className="flex flex-col my-4 min-h-[80dvh]">
       <div className="flex w-full px-[7%] max-w-5xl justify-between items-center">
         <Link href="/">Back in Home</Link>
       </div>
