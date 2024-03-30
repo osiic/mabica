9732b9562e9df4efb5a89d851403fc5def65447b
osiic <osiic.offcl@gmail.com>
2024-03-30 14:35:42 -0800

    feat(login): add link to back in home [/admin,/login]

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function SignInPage() {
   return (
     <div className="flex h-screen">
+      <div className="flex w-full px-[7%] max-w-5xl justify-between items-center">
+        <Link href="/">Back in Home</Link>
+      </div>
       <div className=" m-auto">
         <SignIn />
       </div>
