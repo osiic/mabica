bff5c49a6a93af94c83a3f13b04df9439000ec7b
osiic <osiic.offcl@gmail.com>
2024-03-30 17:25:44 -0800

    fix(login): pks

--- app/(auth)/(authorize)/layout.tsx
+++ app/(auth)/(authorize)/layout.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
 
 export default async function RootLayout({
@@ -14,16 +15,9 @@ export default async function RootLayout({
     `${process.env.NEXT_PUBLIC_HOST}/api/members?id=${userId}`,
   );
   const { data } = await res.json();
-  console.log(data);
 
   // jika status nya tidak ada kembalikan not founs
-  if (!data) {
-    return (
-      <section className="flex h-[100dvh] justify-center items-center font-semibold text-4xl lg:text-5xl">
-        <h1>Anda Siapa kok ikutan mau masuk.</h1>
-      </section>
-    );
-  }
+  if (!data) return notFound();
 
   // jika status nya ada kembalikan ini
   return <>{children}</>;
