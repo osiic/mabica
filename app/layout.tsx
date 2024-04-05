8eb9265019189a34430d92c5646292ebd5ecdd8e
osiic <osiic.offcl@gmail.com>
2024-04-05 15:08:01 -0800

    re(sroll snap): change home srolle to scorlle snap

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,16 +8,27 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Mabica",
-  description: "just",
+  description: `Mabica (Mari Bikin Cerita) adalah perkumpulan orang gabut yang mungkin membuat cerita supaya tidak "rin udah makan" atau "sehat?" saat bermain discord.`,
+  icons: {
+    icon: '/mabica-icon.png',
+    shortcut: '/mabica-icon.png',
+    apple: '/mabica-icon.png',
+    other: {
+      rel: 'mabica-icon',
+      url: '/mabica-icon.png',
+    },
+  },
 };
+
 export const dynamic = "force-dynamic";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={inter.className}>{children}</body>
     </html>
   );
